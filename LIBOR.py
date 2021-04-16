8fa6ef0bc5940bc723defbd0e492efdf59841e80
willempretorius <61142161+wlpretorius@users.noreply.github.com>
2021-04-16 12:02:36 +0200

    Update LIBOR.py

--- LIBOR.py
+++ LIBOR.py
@@ -60,7 +60,7 @@ if page == "About":
                   base64_pdf = base64.b64encode(f.read()).decode('utf-8')
             pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
             st.markdown(pdf_display, unsafe_allow_html=True)
-        st.write(show_pdf("DFCU_Instructions_on_GUI.pdf"))
+        show_pdf("DFCU_Instructions_on_GUI.pdf")
     
     st.header("More about Streamlit")                        
     st.markdown("Official documentation of **[Streamlit](https://docs.streamlit.io/en/stable/getting_started.html)**")
